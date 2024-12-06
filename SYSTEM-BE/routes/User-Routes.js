0243202792c966fc0d5282931187d0c53359a2ac
aldyneronqs <aldyneronquillo91403@gmail.com>
2024-12-06 15:25:37 +0800

    first commit

---
+++ SYSTEM-BE/routes/User-Routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+// Express routing component
+const router = express.Router();
+const userController = require("../controllers/User-Controllers.js");
+const { verify } = require("../auth.js");
+
+// User Registration 
+router.post("/register", userController.registerUser);
+
+
+// User Login
+router.post("/login", userController.loginUser);
+
+
+// Check if email exists
+router.post("/check-email", userController.checkEmail);
+
+// Check User Details
+router.post("/details", verify, userController.getProfile);
+
+// Check User Details
+router.post("/enroll", verify, userController.enroll);
+
+
+module.exports = router;
+ 
